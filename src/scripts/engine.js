6f77dfe9d2f143224deeb49edaf56e67d84f5145
arthursiq5 <arthur.siq5@gmail.com>
2024-02-29 23:04:36 -0300

    Adicionada listagem de cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -25,5 +25,34 @@ const players = {
     computerBOX: document.getElementById('computer-cards'),
 }
 
+const getImagePath = (imgName) => `src/assets/icons/${imgName}.png`;
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: getImagePath('dragon'),
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: getImagePath('magician'),
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: getImagePath('exodia'),
+        WinOf: [0],
+        LoseOf: [1],
+    }
+];
+
 (function() {
 })();
